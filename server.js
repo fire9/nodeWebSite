370291659dfea401aa38dc80d1d4a9051dd9f822
fire9 <fire9@me.com>
2018-09-09 06:43:21 +0800

    run server with express

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 // server.js
 // import the http module
-var http = require('http');
+// var http = require('http');
 
 // handle sending requests and returning responses
-function handleRequests(req, res) {
+// function handleRequests(req, res) {
     // return string
-    res.end('Hello, Node.JS');
-}
+    // res.end('Hello, Node.JS');
+//}
 
 // create the server
-var server = http.createServer(handleRequests);
+// var server = http.createServer(handleRequests);
 
 
 // start server and listen on port number
-server.listen(7788, function() {
+// server.listen(7788, function() {
+//     console.log('Server is running on port 7788');
+// });
+/**
+ * create server with express.js
+ */
+const express = require('express');
+const app = express();
+const host = '127.0.0.1';
+const port = 7788;
+
+
+// run the server
+app.listen(port, host, function() {
     console.log('Server is running on port 7788');
+});
+
+// add route function
+app.get('/', function(req, res) {
+    res.end('Hello, Express!');
 });
